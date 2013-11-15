ef4d933c45c1c3d89067db9105688088b0d2f159
ysarazin <ys@onlineville.com>
2013-11-15 11:25:14 +0100

    Update Gtld_nicline.php

--- Templates/Gtld_nicline.php
+++ Templates/Gtld_nicline.php
@@ -72,46 +72,90 @@ class Template_Gtld_nicline extends AbstractTemplate
     {
         $ResultSet = $WhoisParser->getResult();
 
+        $ResultSet->created = preg_replace('/:\d\d\d/', '', $ResultSet->created);
+        $ResultSet->expires = preg_replace('/:\d\d\d/', '', $ResultSet->expires);
+        
         foreach ($ResultSet->contacts as $contactType => $contactArray) {
             foreach ($contactArray as $contactObject) {
                 $filteredAddress = array_map('trim', explode("\n", trim($contactObject->address)));
 
-                if (sizeof($filteredAddress) === 4) {
-                    $contactObject->name = $filteredAddress[0];
-                    $contactObject->address = $filteredAddress[1];
-                    $contactObject->city = $filteredAddress[2];
-                    $contactObject->country = $filteredAddress[3];
-                } else {
-                    preg_match('/(?>[\x20\t]*)(.*)(?>[\x20\t]{1,})(.*@.*)/i', $filteredAddress[0], $matches);
+                preg_match('/(?>[\x20\t]*)(.*)(?>[\x20\t]{1,})\((.*)\)/i', $filteredAddress[0], $matches);
 
+                if (sizeof($matches) === 0) {
+                    $contactObject->name = $filteredAddress[0];
+                } else {
+                    if (isset($matches[1])) {
+                        $contactObject->name = trim($matches[1]);
+                    }
+
+                    if (isset($matches[2])) {
+                        $contactObject->handle = trim($matches[2]);
+                    }
+                }
+
+                if ($contactType == 'owner') {
+                    $contactObject->email = $filteredAddress[1];
+                    $contactObject->email = $filteredAddress[2];
+                    $contactObject->address = $filteredAddress[3];
+
+                    preg_match('/(?>[\x20\t]*)(.*)(?>[\x20\t]{1,})(.*)/i', $filteredAddress[4], $matches);
                     if (sizeof($matches) === 0) {
-                        $contactObject->name = $filteredAddress[0];
+                        $contactObject->city = $filteredAddress[4];
                     } else {
                         if (isset($matches[1])) {
-                            $contactObject->name = trim($matches[1]);
+                            $contactObject->city = trim($matches[1]);
                         }
 
                         if (isset($matches[2])) {
-                            $contactObject->email = trim($matches[2]);
+                            $contactObject->state = trim($matches[2]);
                         }
                     }
 
-                    $contactObject->organization = $filteredAddress[1];
-                    $contactObject->address = $filteredAddress[2];
-                    $contactObject->city = $filteredAddress[3];
-                    $contactObject->country = $filteredAddress[4];
-
-                    if (isset($filteredAddress[5])) {
-                        preg_match('/(?>[\x20\t]*)(.*?)(?>[\x20\t]{1,})Fax -- (.+)/i', $filteredAddress[5], $matches);
-
+                    preg_match('/(?>[\x20\t]*)(.*)(?>[\x20\t]{1,})(\w\w)/i', $filteredAddress[5], $matches);
+                    if (sizeof($matches) === 0) {
+                        $contactObject->zipcode = $filteredAddress[5];
+                    } else {
                         if (isset($matches[1])) {
-                            $contactObject->phone = $matches[1];
+                            $contactObject->zipcode = trim($matches[1]);
                         }
 
                         if (isset($matches[2])) {
-                            $contactObject->fax = $matches[2];
+                            $contactObject->country = trim($matches[2]);
                         }
                     }
+
+                    $contactObject->phone = $filteredAddress[6];
+                } else {
+                    $contactObject->email = $filteredAddress[1];
+                    $contactObject->address = $filteredAddress[2];
+
+                    preg_match('/(?>[\x20\t]*)(.*)(?>[\x20\t]{1,})(.*)/i', $filteredAddress[3], $matches);
+                    if (sizeof($matches) === 0) {
+                        $contactObject->city = $filteredAddress[3];
+                    } else {
+                        if (isset($matches[1])) {
+                            $contactObject->city = trim($matches[1]);
+                        }
+
+                        if (isset($matches[2])) {
+                            $contactObject->state = trim($matches[2]);
+                        }
+                    }
+
+                    preg_match('/(?>[\x20\t]*)(.*)(?>[\x20\t]{1,})(\w\w)/i', $filteredAddress[4], $matches);
+                    if (sizeof($matches) === 0) {
+                        $contactObject->zipcode = $filteredAddress[4];
+                    } else {
+                        if (isset($matches[1])) {
+                            $contactObject->zipcode = trim($matches[1]);
+                        }
+
+                        if (isset($matches[2])) {
+                            $contactObject->country = trim($matches[2]);
+                        }
+                    }
+
+                    $contactObject->phone = $filteredAddress[5];
                 }
             }
         }
